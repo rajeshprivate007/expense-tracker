fea843963ebd7ab8c713e7bd5ffacff8689b1841
rajeshprivate007 <rajesh.sahu.private@gmail.com>
2026-07-12 12:59:08 +0530

    Updated Terraform root main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 module "resource_group" {
   source   = "./modules/resource_group"
-  name     = var.resource_group.rg.name
-  location = var.resource_group.rg.location
-  tags     = var.resource_group.rg.tags
-  
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.common_tags
 }
