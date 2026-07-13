cda58b1f39a7e3b42d385ff65be037b4714166d0
rajeshprivate007 <rajesh.sahu.private@gmail.com>
2026-07-13 22:39:14 +0530

    Fixed vnet terraform code

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,17 +7,13 @@ module "resource_group" {
 }
 
 # Terraform configuration for Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = var.vnet_address_space
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name             = var.aks_subnet_name
-    address_prefixes = var.subnet_cidr
-  }
-
-  tags = var.common_tags
+module "virtual_network" {
+  source               = "./modules/virtual_network"
+  resource_group_name  = module.resource_group.name
+  location             = module.resource_group.location
+  vnet_name            = var.vnet_name
+  vnet_address_space   = var.vnet_address_space
+  aks_subnet_name      = var.aks_subnet_name
+  subnet_cidr          = var.subnet_cidr
+  common_tags          = var.common_tags
 }
